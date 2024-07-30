48c81cfdab7dc3444fa066dacd78acd1dc5d78dc
Efendi Salim <efendy.s.89@gmail.com>
2024-07-30 17:03:43 +0700

    add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,58 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueJsx(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'robots.txt'],
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'Zeals',
+                icons: [
+                    {
+                        src: '/icons/android-icon-36x36.png',
+                        sizes: '36x36',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-icon-48x48.png',
+                        sizes: '48x48',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-icon-72x72.png',
+                        sizes: '72x72',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-icon-96x96.png',
+                        sizes: '96x96',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-icon-144x144.png',
+                        sizes: '144x144',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/android-icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
